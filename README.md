4d7ba09ee70e788984d1f52ab019aecf618b7306
Paciente8159 <Paciente8159@users.noreply.github.com>
2021-03-29 18:28:19 +0100

    Moved key definition to wp-config
    -moved key setting to wp-config.php

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 A simple plugin to add Google recaptcha v3 to your blog comments
 
 # Configuration and usage
-Download the plugin and unzip it.
-Open jcem-blog-comment-recaptcha.php and modify lines 33 and 34 to define your site key and secret
-Zip all the files and upload the plugin.
-You're done. The submited comments that fail to check the recaptcha checking are sent to the spam separator so you can later filter them.
+Download the plugin and install it.
+Open your wp-config.php file and define RECAPTCHA_SITE_KEY and RECAPTCHA_SECRET_KEY like this
+
+```php
+define('RECAPTCHA_SITE_KEY', 'YOUR KEY');
+define('RECAPTCHA_SECRET_KEY', 'YOUR SECRET');
+```
--- jcem-blog-comment-recaptcha.php
+++ jcem-blog-comment-recaptcha.php
@@ -30,72 +30,72 @@ if (!defined('ABSPATH')) {
     die;
 }
 
-define('RECAPTCHA_SITE_KEY', 'YOUR_SITE_KEY');
-define('RECAPTCHA_SECRET_KEY', 'YOUR_SECRET_KEY');
-
-add_action('wp_head', function () {
-    if (get_post_type() == 'post') {
-?>
-        <script src="https://www.google.com/recaptcha/api.js?render=<?php echo RECAPTCHA_SITE_KEY; ?>" async></script>
-    <?php
-    }
-});
-
-add_filter('comment_form_submit_button', function ($submit_button, $args) {
-    $new_button = sprintf(
-        '<input name="%1$s" type="submit" id="%2$s" class="%3$s" value="%4$s" onclick="recaptchaSubmit(this,event)"/>',
-        esc_attr($args['name_submit']),
-        esc_attr($args['id_submit']),
-        esc_attr($args['class_submit']),
-        esc_attr($args['label_submit'])
-    );
-    return $new_button;
-}, 10, 2);
-
-add_filter('comment_form_submit_field', function ($submit_field, $args) {
-    $submit_id = $args['id_submit'];
-    $submit_form = $args['id_form'];
-    $submit_field .= sprintf('<input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response" data-submit="%s" data-form="%s" value="">', $submit_id, $submit_form);
-
-    return $submit_field;
-}, 10, 2);
-
-
-add_filter('pre_comment_approved', function ($approved, $commentdata) {
-    $api = 'https://www.google.com/recaptcha/api/siteverify';
-    $secret = RECAPTCHA_SECRET_KEY;
-    $response = isset($_POST['g-recaptcha-response']) ? $_POST['g-recaptcha-response'] : '';
-    $query = sprintf('%s?secret=%s&response=%s&remoteip=%s', $api, $secret, $response, $_SERVER['REMOTE_ADDR']);
-    $validation = file_get_contents($query);
-    $valid = json_decode($validation, false);
-
-    if ($valid->success == true && $valid->score <= 0.5) {
-        return 'spam';
-    }
+if (defined('RECAPTCHA_SITE_KEY') && defined('RECAPTCHA_SECRET_KEY')) {
     
-    return $approved;
-}, 10, 2);
-
-add_action('wp_footer', function () {
-    ?>
-    <script type="text/javascript" async>
-        function recaptchaSubmit(b, e) {
-            e.preventDefault();
-            grecaptcha.ready(function() {
-                grecaptcha.execute('<?php echo RECAPTCHA_SITE_KEY; ?>', {
-                    action: 'submit'
-                }).then(function(token) {
-                    var r = document.getElementById("g-recaptcha-response");
-                    r.value = token;
-                    if (b.id == 'submit') {
-                        b.removeAttribute("name");
-                        b.id = "recaptcha-" + b.id;
-                    }
-                    var f = document.getElementById(r.getAttribute('data-form'));
-                    f.submit();
-                });
-            });
+    add_action('wp_head', function () {
+        if (get_post_type() == 'post') {
+?>
+            <script src="https://www.google.com/recaptcha/api.js?render=<?php echo RECAPTCHA_SITE_KEY; ?>" async></script>
+        <?php
         }
-    </script>
+    });
+
+    add_filter('comment_form_submit_button', function ($submit_button, $args) {
+        $new_button = sprintf(
+            '<input name="%1$s" type="submit" id="%2$s" class="%3$s" value="%4$s" onclick="recaptchaSubmit(this,event)"/>',
+            esc_attr($args['name_submit']),
+            esc_attr($args['id_submit']),
+            esc_attr($args['class_submit']),
+            esc_attr($args['label_submit'])
+        );
+        return $new_button;
+    }, 10, 2);
+
+    add_filter('comment_form_submit_field', function ($submit_field, $args) {
+        $submit_id = $args['id_submit'];
+        $submit_form = $args['id_form'];
+        $submit_field .= sprintf('<input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response" data-submit="%s" data-form="%s" value="">', $submit_id, $submit_form);
+
+        return $submit_field;
+    }, 10, 2);
+
+
+    add_filter('pre_comment_approved', function ($approved, $commentdata) {
+        $api = 'https://www.google.com/recaptcha/api/siteverify';
+        $secret = RECAPTCHA_SECRET_KEY;
+        $response = isset($_POST['g-recaptcha-response']) ? $_POST['g-recaptcha-response'] : '';
+        $query = sprintf('%s?secret=%s&response=%s&remoteip=%s', $api, $secret, $response, $_SERVER['REMOTE_ADDR']);
+        $validation = file_get_contents($query);
+        $valid = json_decode($validation, false);
+
+        if ($valid->success == true && $valid->score <= 0.5) {
+            return 'spam';
+        }
+
+        return $approved;
+    }, 10, 2);
+
+    add_action('wp_footer', function () {
+        ?>
+        <script type="text/javascript" async>
+            function recaptchaSubmit(b, e) {
+                e.preventDefault();
+                grecaptcha.ready(function() {
+                    grecaptcha.execute('<?php echo RECAPTCHA_SITE_KEY; ?>', {
+                        action: 'submit'
+                    }).then(function(token) {
+                        var r = document.getElementById("g-recaptcha-response");
+                        r.value = token;
+                        if (b.id == 'submit') {
+                            b.removeAttribute("name");
+                            b.id = "recaptcha-" + b.id;
+                        }
+                        var f = document.getElementById(r.getAttribute('data-form'));
+                        f.submit();
+                    });
+                });
+            }
+        </script>
 <?php
-});
+    });
+}
